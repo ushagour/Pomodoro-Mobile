a776c7e6d7b5cf85594220fce0b74fac15beafad
ali ouchagour <ali.ouchagour01@gmail.com>
2023-07-01 02:43:48 +0100

    adding write new task wrrappaer with compatible styling and avoiding the keyboard to be aponed

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 
 import React from 'react';
-import { StyleSheet, Text, View, FlatList } from 'react-native';
+import { StyleSheet, Text, View, FlatList, Platform,KeyboardAvoidingView,TextInput,TouchableOpacity } from 'react-native';
 import Task from "./components/Task.js";
 export default function App() {
 
@@ -10,9 +10,33 @@ export default function App() {
         <View style={styles.tasksWrapper}>
             <Text style={styles.headerText}>To day's Tasks :</Text>
             <View style={styles.items}>
-                <Task  title={'first task'}/>
+                <Task  title={'Lorem ipsum dolor sit amet'}/>
             </View>
         </View>
+
+
+
+
+    <KeyboardAvoidingView 
+    
+    behavior={Platform.OS ==="ios"?"padding":"height"}
+    style={styles.WriteTaskWrapper}
+    
+    
+  >
+
+<TextInput style={styles.input} placeholder={'write a task '}/>
+<TouchableOpacity>
+<View style={styles.addWrapper}>
+
+<Text style={styles.TextAdd}>+</Text>
+</View>
+</TouchableOpacity>
+
+</KeyboardAvoidingView>
+
+
+
     </View>
   );
 }
@@ -35,5 +59,35 @@ const styles = StyleSheet.create({
   items:{
     marginTop:30,
 
-  }
+  },
+  WriteTaskWrapper:{
+    position:"absolute",
+    bottom:60,
+    width:"100%",
+    flexDirection:"row",
+    justifyContent:'space-around',
+    alignItems:"center",
+
+  },input:{
+    paddingVertical:15,
+    paddingHorizontal:15,
+    width:250,
+    backgroundColor:"#fff",
+    borderRadius:60,
+    borderColor:"#C0C0C0",
+    borderWidth:1,
+  },addWrapper:{
+    height:60,
+    width:60,
+    borderRadius:60,
+    backgroundColor:"#fff",
+    justifyContent:"center",
+    alignItems:"center",
+    borderColor:"#C0C0C0",
+    borderWidth:1,
+
+    
+
+  },
+  TextAdd:{},
 });
